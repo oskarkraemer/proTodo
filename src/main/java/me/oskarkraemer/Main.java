38b4c294b0933bf8521d83308d282d8944a9eafc
Oskar Krämer <42745862+oskarkraemer@users.noreply.github.com>
2024-03-25 16:50:21 +0100

    Initial commit

---
+++ src/main/java/me/oskarkraemer/Main.java
@@ -0,0 +1,11 @@
+package me.oskarkraemer;
+
+import com.formdev.flatlaf.FlatDarculaLaf;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        FlatDarculaLaf.setup();
+    }
+}
