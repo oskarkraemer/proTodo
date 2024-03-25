4f20dc5d0f6f387478579f4d12e8e187b4811acd
Oskar Krämer <42745862+oskarkraemer@users.noreply.github.com>
2024-03-25 23:18:24 +0100

    make Consolas default font

--- src/main/java/me/oskarkraemer/Main.java
+++ src/main/java/me/oskarkraemer/Main.java
@@ -4,9 +4,13 @@ import com.formdev.flatlaf.FlatDarculaLaf;
 import me.oskarkraemer.TodoList.TodoList;
 import me.oskarkraemer.TodoList.TodoListParser;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
         FlatDarculaLaf.setup();
+        UIManager.getLookAndFeelDefaults().put("defaultFont", new Font("Consolas", Font.PLAIN, 12));
 
         TodoList todoList = TodoListParser.parseMarkdown("# ToDo: ProjectX\n" +
                 "## Metadata\n" +
